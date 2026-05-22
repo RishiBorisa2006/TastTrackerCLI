11c7b52302420139a6f8315650ff336a43009927
RishiBorisa2006 <rishi.borisa.dev@gmail.com>
2026-05-22 08:38:03 +0530

    addest just starting foundation (switch)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-
+        if(args.length == 0){
+            System.out.println("Please provide a valid argument.");
+            return;
+        }
+        String input = args[0];
+        switch (input) {
+            case "add":
+                System.out.println("You chose add!");
+                break;
+            case "list":
+                System.out.println("You chose list!");
+                break;
+            case "delete":
+                System.out.println("You chose delete!");
+                break;
+            default:
+                System.out.println("Invalid input!");
+                break;
+        }
     }
 }
